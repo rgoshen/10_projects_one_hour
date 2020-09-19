e63d0dbe475ea6ab030df6c9004a7e36fc6558b9
rgoshen <rick.goshen@gmail.com>
2020-09-18 19:56:12 -0700

    added background color change button

---
+++ scripts/bg_color.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+  const bgColorClass = [
+    "bg-primary",
+    "bg-secondary",
+    "bg-success",
+    "bg-danger",
+    "bg-warning",
+    "bg-info",
+    "bg-light",
+    "bg-dark",
+  ];
+  let index = 0;
+
+  $("#bgColor").click(() => {
+    if (index === bgColorClass.length) {
+      $("body").removeClass(bgColorClass[index - 1]);
+      index = 0;
+    }
+
+    if ($("body").hasClass(bgColorClass[index - 1])) {
+      $("body").removeClass(bgColorClass[index - 1]);
+    }
+
+    $("body").addClass(bgColorClass[index]);
+    index++;
+  });
+});
